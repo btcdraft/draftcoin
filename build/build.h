7d0ce4cabd7d7b4003b34d62a0cb04519c1f17c5
Tiranad <james@xmc.com>
2018-01-19 13:27:38 -0700

    Windows Installer - DraftCoin Wallet

--- build/build.h
+++
@@ -1,2 +0,0 @@
-// No build information available
-#define BUILD_DATE "2017-11-29 17:36:58 -0500"
--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/DraftCoin.ico
-
-convert ../../src/qt/res/icons/DraftCoin-16.png ../../src/qt/res/icons/DraftCoin-32.png ../../src/qt/res/icons/DraftCoin-48.png ${ICON_DST}
