89d8e36491973ede27631f6dcf7a85a9a3411b5c
Tiranad <james@xmc.com>
2018-03-27 23:45:13 -0600

    update to version 1.0.0.5

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
                          +     400 * CLIENT_VERSION_REVISION
-                         +       4 * CLIENT_VERSION_BUILD;
+                         +       5 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
